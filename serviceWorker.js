34df603d396480bc489624c26cb1f750f6b1b201
Albert Albala <albert@mediatribe.net>
2023-05-19 16:06:56 -0400

    finalize

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-onst staticDevCoffee = "dev-coffee-site-v1"
+const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
   "/",
   "/index.html",
@@ -19,6 +19,14 @@ self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
       cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
     })
   )
 })
